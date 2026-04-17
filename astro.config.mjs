cbffeebe61bc49d19b556cba5d45cff4b03da2ca
hamb1y <rishi.s.malnad@gmail.com>
2026-04-17 11:34:43 +0530

    mb?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import { z } from 'astro/zod';
+import { defineConfig, envField } from 'astro/config';
 
 import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
+  output: 'static',
   integrations: [svelte()],
   env: {
     schema: {
-      PUBLIC_PAYLOAD_API_URL: z.string().url().optional().default('http://localhost:3000'),
+      PUBLIC_PAYLOAD_API_URL: envField.string({
+        context: 'client',
+        access: 'public',
+        optional: true,
+        default: 'http://localhost:3000',
+      }),
     },
   },
 });
